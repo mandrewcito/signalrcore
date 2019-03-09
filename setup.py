e7fb0cc7ab79c01db9ed27c56da517a562139854
Andrés Baamonde Lozano <anbaalo@gmail.com>
2019-03-09 22:18:06 +0100

    new version

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="signalrcore",
-    version="0.3.1",
+    version="0.4.0",
     author="mandrewcito",
     author_email="anbaalo@gmail.com",
     description="A Python SignalR Core client ",
-    keywords="signalr core client",
+    keywords="signalr core client, with invocation auth and streamming. ",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mandrewcito/signalrcore",
