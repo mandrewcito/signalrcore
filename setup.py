927dff9d7d862a56db1f8e7317a45e5f15b0aea0
Andrés Baamonde <anbaalo@gmail.com>
2020-07-30 18:16:23 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3"
     ],
     install_requires=[
-        "requests>=2.21.0",
-        "websocket-client>=0.55.0"
+        "requests>=2.24.0",
+        "websocket-client>=0.57.0"
     ]
 )
