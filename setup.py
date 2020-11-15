0ac850425211de0cb83e80ee5e892cdc9884f8b6
mandrewcito <anbaalo@gmail.com>
2020-11-15 15:52:12 +0100

    feat(msgpack): dependencies

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     ],
     install_requires=[
         "requests>=2.22.0",
-        "websocket-client>=0.54.0"
+        "websocket-client>=0.54.0",
         "msgpack>=1.0.0"
     ]
 )
