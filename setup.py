75d14c1777ec950dc56db24c7c444860b6f2de36
Andrés Baamonde Lozano <mandrewcito@users.noreply.github.com>
2021-03-08 08:24:50 +0100

    feat(handshake): Azure json fix (#53)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="signalrcore",
-    version="0.8.9",
+    version="0.9.0",
     author="mandrewcito",
     author_email="anbaalo@gmail.com",
     description="A Python SignalR Core client(json and messagepack), with invocation auth and two way streaming. Compatible with azure / serverless functions. Also with automatic reconnect and manually reconnect.",
