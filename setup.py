c248cef2edcbec2cdc9d7fd3cb8b97470807ae01
Andrés Baamonde Lozano <mandrewcito@users.noreply.github.com>
2021-03-08 11:04:41 +0100

    fix(setup): requirements (#55)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="signalrcore",
-    version="0.9.0",
+    version="0.9.1",
     author="mandrewcito",
     author_email="anbaalo@gmail.com",
     description="A Python SignalR Core client(json and messagepack), with invocation auth and two way streaming. Compatible with azure / serverless functions. Also with automatic reconnect and manually reconnect.",
@@ -19,8 +19,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6"
     ],
     install_requires=[
-        "requests>=2.22.0",
-        "websocket-client>=0.54.0",
-        "msgpack>=1.0.0"
+        "requests==2.25.1",
+        "websocket-client==0.54.0",
+        "msgpack==1.0.2"
     ]
 )
