0792bac6d56e499256948c6f2277a4b8d1ea959c
Andrés Baamonde Lozano <mandrewcito@users.noreply.github.com>
2022-04-05 23:12:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="signalrcore",
-    version="0.9.4",
+    version="0.9.5",
     author="mandrewcito",
     author_email="anbaalo@gmail.com",
     description="A Python SignalR Core client(json and messagepack), with invocation auth and two way streaming. Compatible with azure / serverless functions. Also with automatic reconnect and manually reconnect.",
